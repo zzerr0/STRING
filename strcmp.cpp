f95d441ab6a7af8fceeb3d39f90b482be3e3d9a0
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2021-05-21 07:14:03 +0530

    Create strcmp.cpp

---
+++ strcmp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main(){
+char ab[] = "330";
+char ba[] = "303";
+
+int res =  strcmp(ab,ba);
+cout<<"Res is "<<res<<endl;
+return 0;
+}
